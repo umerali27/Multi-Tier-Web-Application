074f4e6aa46f4e8346a459295f3da16a6a0feb99
Umer Ali <umerali91105@gmail.com>
2026-01-17 20:54:42 +0300

    Update app.py

--- Multi-tier-App/Backend/app.py
+++ Multi-tier-App/Backend/app.py
@@ -1,1 +1,15 @@
+from flask import Flask, jsonify
+app = Flask(__name__)
+
+tasks = [
+    {"id": 1, "title": "Learn Docker"},
+    {"id": 2, "title": "Build Multi-Tier App"}
+]
+
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+    return jsonify(tasks)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
 
